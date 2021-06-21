ff06f6dcd7fbd5856584e7dc406a44e43f5cc6bd
Bruno Silva Freire <brunorbsf@gmail.com>
2021-06-21 19:08:42 -0400

    Oh yeah, php

---
+++ index.php
@@ -0,0 +1,16 @@
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Avaliação SOCPS II</h2>
+    <h1>Bruno Ribeiro Braga Silva Freire</h1>
+    <p><?php
+    echo "Today is " . date("Y/m/d") . "<br>";
+    ?></p>
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '342d837f3708c9655c9432d80f370fb875228325',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '342d837f3708c9655c9432d80f370fb875228325',
+            'dev_requirement' => false,
+        ),
+        'phpunit/php-timer' => array(
+            'pretty_version' => '1.0.9',
+            'version' => '1.0.9.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpunit/php-timer',
+            'aliases' => array(),
+            'reference' => '3dcf38ca72b158baf0bc245e9184d3fdffa9c46f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
